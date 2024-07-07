d8c64cc8dcba873bfcd8f8574320234259576802
Rajneesh Maurya <rajneesh12084@github.com>
2024-07-07 19:28:46 +0530

    updated

--- models.py
+++ models.py
@@ -1,10 +1,17 @@
-# app/models.py
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Portfolio(db.Model):
+    # Unique identifier for each portfolio item
     id = db.Column(db.Integer, primary_key=True)
+    
+    # Title of the portfolio item
     title = db.Column(db.String(100), nullable=False)
+    
+    # Description of the portfolio item
     description = db.Column(db.String(200), nullable=False)
+    
+    # URL to an image representing the portfolio item
     image_url = db.Column(db.String(200), nullable=False)
